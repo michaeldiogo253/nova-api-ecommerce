391145c7b80725faec6bceba3c46d89b394ec4be
Michael Diogo Fagundes <michael@petize.com.br>
2023-03-24 09:45:14 -0300

    refactor! adicionado @Bean para Spring injetar CountDownLatch

--- src/main/java/michaelstudy/ecommercenewapi/EcommerceNewApiApplication.java
+++ src/main/java/michaelstudy/ecommercenewapi/EcommerceNewApiApplication.java
@@ -2,12 +2,21 @@ package michaelstudy.ecommercenewapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class EcommerceNewApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EcommerceNewApiApplication.class, args);
+	}
+
+	@Bean
+	public CountDownLatch countDownLatch() {
+		return new CountDownLatch(1);
 	}
 
 }
